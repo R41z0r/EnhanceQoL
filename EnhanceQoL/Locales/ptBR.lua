2fe6e99cd38118906886f1e6e40e4cd92b00c1dd
R41z0r <philipp.barth@cloudra.de>
2024-12-12 14:48:27 +0100

    Removed Whitespaces

--- EnhanceQoL/Locales/ptBR.lua
+++ EnhanceQoL/Locales/ptBR.lua
@@ -24,7 +24,7 @@ L["showCatalystChargesOnCharframe"] = "Mostrar cargas do catalisador no quadro d
 L["showIlvlOnBagItems"] = "Exibir o nível de item no equipamento em todas as bolsas"
 L["showDurabilityOnCharframe"] = "Exibir durabilidade no quadro de equipamento do personagem"
 L["hideOrderHallBar"] = "Ocultar barra de comando do Salão da Ordem"
-    
+
 L["deleteItemFillDialog"] = "Adicionar \"" .. DELETE_ITEM_CONFIRM_STRING ..
                                 "\" ao \"Popup de confirmação de exclusão\""
 L["autoChooseQuest"] = "Aceitar e completar missões automaticamente"
