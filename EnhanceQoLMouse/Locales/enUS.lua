29c272d4112ec97d3f0cf265e438318ab4744881
R41z0r <philipp.barth@cloudra.de>
2025-05-04 14:41:42 +0200

    Base Language for mouse

--- EnhanceQoLMouse/Locales/enUS.lua
+++ EnhanceQoLMouse/Locales/enUS.lua
@@ -1,1 +1,8 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("EnhanceQoL_Aura", "enUS", true) -- “true” = default / fallback
+
+L["mouseRingEnabled"] = "Enable visible ring around the mouse cursor"
+L["mouseTrailEnabled"] = "Enable visible trail while moving the mouse cursor"
+L["Trailinfo"] = "Depending on your hardware this could have a performance impact"
+L["Ring Color"] = "Ring color"
+L["Trail Color"] = "Trail color"
+L["mouseTrailDensity"] = "Mouse trail density"
