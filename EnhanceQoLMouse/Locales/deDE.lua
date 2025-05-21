5d6ec44a4572c2a709436c234aa6a17e8a5296c0
R41z0r <6240199+R41z0r@users.noreply.github.com>
2025-05-21 23:30:42 +0200

    Add mouse ring size setting

--- EnhanceQoLMouse/Locales/deDE.lua
+++ EnhanceQoLMouse/Locales/deDE.lua
@@ -2,3 +2,4 @@ local L = LibStub("AceLocale-3.0"):NewLocale("EnhanceQoL_Mouse", "deDE")
 if not L then return end
 
 --@localization(locale="deDE", namespace="Mouse", format="lua_additive_table")@
+L["mouseRingSize"] = "Ring size"
--- EnhanceQoLMouse/Locales/esMX.lua
+++ EnhanceQoLMouse/Locales/esMX.lua
@@ -2,3 +2,4 @@ local L = LibStub("AceLocale-3.0"):NewLocale("EnhanceQoL_Mouse", "esMX")
 if not L then return end
 
 --@localization(locale="esMX", namespace="Mouse", format="lua_additive_table")@
+L["mouseRingSize"] = "Ring size"
