3d087aae3e2ebc1e5519663fa2096d701e77b9e7
R41z0r <6240199+R41z0r@users.noreply.github.com>
2025-06-15 20:26:39 +0200

    Convert LayoutTools locales to AceLocale

--- EnhanceQoLLayoutTools/Locales/enUS.lua
+++ EnhanceQoLLayoutTools/Locales/enUS.lua
@@ -1,17 +1,9 @@
-local parentAddonName = "EnhanceQoL"
-local addonName, addon = ...
-if _G[parentAddonName] then
-	addon = _G[parentAddonName]
-else
-	error(parentAddonName .. " is not loaded")
-end
-local L = addon.LLayoutTools
+local L = LibStub("AceLocale-3.0"):NewLocale("EnhanceQoL_LayoutTools", "enUS", true) -- "true" = default / fallback
 
 L["Move"] = "Layout Tools"
 
 L["uiScalerPlayerSpellsFrameMove"] = "Enable to move " .. PLAYERSPELLS_BUTTON
 L["uiScalerPlayerSpellsFrameEnabled"] = "Enable to Scale the " .. PLAYERSPELLS_BUTTON
-
 
 L["uiScalerCharacterFrameMove"] = "Enable to move " .. CHARACTER_BUTTON
 L["uiScalerCharacterFrameEnabled"] = "Enable to Scale the " .. CHARACTER_BUTTON
