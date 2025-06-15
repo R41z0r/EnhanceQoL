6bf26e32f0b4c1870562eda642ec322d0ae6370c
R41z0r <6240199+R41z0r@users.noreply.github.com>
2025-06-15 06:11:08 +0200

    fix buff tracker UI layout

--- EnhanceQoLAura/BuffTracker.lua
+++ EnhanceQoLAura/BuffTracker.lua
@@ -416,6 +416,7 @@ local function openBuffConfig(catId, id)
 		frame:AddChild(altEdit)
 
 		frame:AddChild(addon.functions.createSpacerAce())
+		frame:DoLayout()
 	end
 
 	rebuild()
@@ -536,6 +537,7 @@ function addon.Aura.functions.buildCategoryOptions(tabContainer, catId, groupTab
 	for specID, val in pairs(cat.allowedSpecs or {}) do
 		if val then specDrop:SetItemValue(specID, true) end
 	end
+	specDrop:SetRelativeWidth(0.5)
 	core:AddChild(specDrop)
 
 	local classDrop = addon.functions.createDropdownAce(L["ShowForClass"], classNames, nil, function(self, event, key, checked)
@@ -547,6 +549,7 @@ function addon.Aura.functions.buildCategoryOptions(tabContainer, catId, groupTab
 	for c, val in pairs(cat.allowedClasses or {}) do
 		if val then classDrop:SetItemValue(c, true) end
 	end
+	classDrop:SetRelativeWidth(0.5)
 	core:AddChild(classDrop)
 
 	local spellEdit = addon.functions.createEditboxAce(L["SpellID"], nil, function(self, _, text)
@@ -634,13 +637,6 @@ function addon.Aura.functions.buildTabContent(tabContainer, catId, scroll, group
 		end)
 		cbSpell.frame:HookScript("OnLeave", function() GameTooltip:Hide() end)
 		row:AddChild(cbSpell)
-
-		local cbMissing = addon.functions.createCheckboxAce(L["buffTrackerShowWhenMissing"], cat.buffs[info.id].showWhenMissing, function(self, _, val)
-			cat.buffs[info.id].showWhenMissing = val
-			updateBuff(catId, info.id)
-		end)
-		cbMissing:SetRelativeWidth(0.1)
-		row:AddChild(cbMissing)
 
 		local upIcon = AceGUI:Create("Icon")
 		upIcon:SetLabel("")
