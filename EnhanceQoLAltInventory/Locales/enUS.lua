b2bfe7b9b8866951cc7f991890ae92afd3ea1832
R41z0r <6240199+R41z0r@users.noreply.github.com>
2025-07-11 11:14:22 +0200

    Add localization framework for AltInventory

---
+++ EnhanceQoLAltInventory/Locales/enUS.lua
@@ -0,0 +1,8 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("EnhanceQoL_AltInventory", "enUS", true)
+
+L["Alt Inventory"] = "Alt Inventory"
+L["By Item"] = "By Item"
+L["By Character"] = "By Character"
+L["Total"] = "Total"
+L["AltInventoryTooltip"] = "|cffeda55fClick|r to toggle Alt Inventory"
+
---
+++ EnhanceQoLAltInventory/Locales/zhCN.lua
@@ -0,0 +1,5 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("EnhanceQoL_AltInventory", "zhCN")
+if not L then return end
+
+--@localization(locale="zhCN", namespace="AltInventory", format="lua_additive_table")@
+
